eae4370a11f508722920258d04d9422829089da0
jannes-m <malnamalja@gmx.de>
2018-11-20 17:22:27 +0000

    update docker test script

--- inst/test-scripts/docker_rqgis3_test.R
+++ inst/test-scripts/docker_rqgis3_test.R
@@ -1,11 +1,12 @@
 library("sf")
 library("raster")
 devtools::load_all()
+qgis_env = set_env()
 setup_linux()
 py_run_string("from pyvirtualdisplay import Display")
 py_run_string("display = Display(visible=False, size=(1024, 768), color_depth=24)")
 py_run_string("display.start()")
-open_app3()
+# open_app3()
 
 qgis_session_info()
 find_algorithms()
