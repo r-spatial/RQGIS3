87f12e631409ccebbab421c0202b4b3441d040a5
pat-s <patrick.schratz@gmail.com>
2019-08-30 20:00:29 +0200

    upd mac_run_qgis.R test script

--- inst/test-scripts/mac_run_qgis.R
+++ inst/test-scripts/mac_run_qgis.R
@@ -3,22 +3,43 @@
 #********************************************************
 
 devtools::load_all()
-use_python("/usr/local/bin/python3")
 
-Sys.setenv(LD_LIBRARY_PATH = paste("/usr/local/Cellar/osgeo-qgis/3.8.0_1/QGIS.app/Contents/MacOS/lib/:/Applications/QGIS.app/Contents/Frameworks/"))
 
-Sys.setenv(PYTHONPATH = "PYTHONPATH=/usr/local/opt/lib/python3.7/site-packages:/usr/local/opt/osgeo-qgis/lib/python3.7/site-packages:/usr/local/opt/osgeo-qgis/QGIS.app/Contents/Resources/python:/usr/local/opt/osgeo-gdal-python/lib/python3.7/site-packages:$PYTHONPATH")
-
+# homebrew ---------------------------------------------------------------------
+Sys.setenv(LD_LIBRARY_PATH = "/usr/local/Cellar/osgeo-qgis/3.8.0_1/QGIS.app/Contents/MacOS/lib/:/Applications/QGIS.app/Contents/Frameworks/")
+Sys.setenv(PYTHONPATH = "/usr/local/opt/lib/python3.7/site-packages:/usr/local/opt/osgeo-qgis/lib/python3.7/site-packages:/usr/local/opt/osgeo-qgis/QGIS.app/Contents/Resources/python:/usr/local/opt/osgeo-gdal-python/lib/python3.7/site-packages:$PYTHONPATH")
 Sys.setenv(QGIS_PREFIX_PATH = "/usr/local/Cellar/osgeo-qgis/3.8.0_1/QGIS.app/Contents/MacOS/")
+# non-homebrew -----------------------------------------------------------------
+
+# Pythonpath is the problem
+# if we take the pythonpath from above, it works
+# /usr/local/opt/osgeo-qgis/lib/python3.7/site-packages/ is missing for non-homebrew
+# this folder contains the PyQt5 python resources and a link to the qgis processing modules
+# Q: Where are the QGIS processing libs??
+Sys.setenv(LD_LIBRARY_PATH = "/Applications/QGIS3.4.app/Contents/MacOS/lib:/Applications/QGIS3.4.app/Contents/Frameworks")
+Sys.setenv(PYTHONPATH = "/Applications/QGIS3.4.app/Contents/Resources/python:$PYTHONPATH")
+Sys.setenv(QGIS_PREFIX_PATH = "/Applications/QGIS3.4.app/Contents/MacOS/")
 Sys.setenv(QGIS_DEBUG=-1)
 
-# reproducing our py_cmd.py
+# homebrew ---------------------------------------------------------------------
+#use_python("/usr/local/bin/python3")
+# non-homebrew -----------------------------------------------------------------
+reticulate::use_python("/Applications/QGIS3.4.app/Contents/Frameworks/Python.framework/Versions/Current/bin/python3", required = TRUE)
+
+
 py_run_string("import os, re, sys")
 py_run_string("from qgis.core import *")
 py_run_string("from osgeo import ogr")
 py_run_string("from PyQt5.QtCore import *")
 py_run_string("from PyQt5.QtGui import *")
 py_run_string("from qgis.gui import *")
+
+# non-homebrew -----------------------------------------------------------------
+
+py_run_string("sys.path.append(r'/Applications/QGIS3.4.app/Contents/Resources/python/plugins')")
+py_run_string("QgsApplication.setPrefixPath(r'/Applications/QGIS3.4.app/Contents/', True)")
+
+# homebrew ---------------------------------------------------------------------
 
 py_run_string("sys.path.append(r'/usr/local/Cellar/osgeo-qgis/3.8.0_1/QGIS.app/Contents/Resources/python/plugins')")
 py_run_string("QgsApplication.setPrefixPath(r'/usr/local/Cellar/osgeo-qgis/3.8.0_1/QGIS.app/Contents', True)")
@@ -27,22 +48,6 @@ py_eval("QgsApplication.showSettings()")
 py_run_string("app = QgsApplication([], True)")
 
 py_run_string("QgsApplication.initQgis()")
-
-py_run_string("from qgis.core import (QgsSettings,
-                       QgsProcessingFeedback,
-                       Qgis,
-                       QgsMessageLog,
-                       QgsApplication,
-                       QgsMapLayer,
-                       QgsProcessingProvider,
-                       QgsProcessingAlgorithm,
-                       QgsProcessingException,
-                       QgsProcessingParameterDefinition,
-                       QgsProcessingParameterEnum,
-                       QgsProcessingOutputVectorLayer,
-                       QgsProcessingOutputRasterLayer,
-                       QgsProcessingOutputMapLayer,
-                       QgsProcessingOutputMultipleLayers)")
 
 py_run_string("from processing.tools.general import algorithmHelp
 from processing.algs.saga.SagaAlgorithmProvider import SagaAlgorithmProvider
